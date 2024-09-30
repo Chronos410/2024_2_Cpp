a0192404454dde725c7beece4ab53273a44d2be4
chronos410 <chronos410@naver.com>
2024-09-30 15:40:22 +0900

    add: week6 추가

---
+++ Cpp_week6/Cpp_week6/week6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct mystruct {
+	int id;
+	int a;
+};
+
+typedef struct {
+	int id;
+	int abc;
+} my_t;
+
+class myclass {
+	int id;
+	int a;
+public:
+	void printID() {
+
+	}
+};
+
+int main() 
+{
+
+}
---
+++ Cpp_week6/Cpp_week6_fanCtrl/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "fanCtrl.h"
+using namespace std;
+
+int main() 
+{
+
+}
